87dd4e59b2b349e872a38468d74892ee3b67c28c
jay-java <jayprajapati.tops@gmail.com>
2024-11-28 16:28:36 +0530

    first lecture

---
+++ CoreJAVA/src/core/P002_DataType.java
@@ -0,0 +1,18 @@
+package core;
+//byte,short,int,long,float,double,boolean,char
+//array,String,class
+public class P002_DataType {
+	public static void main(String[] args) {
+		byte b = 127;//1
+		short s = 12;//2
+		int i = 12;//4
+		long l = 12;//8
+		float f =3.14f;
+		double d = 3.14;
+		boolean bool = true;
+		char c = 'q';
+		
+		int a = 12,b1 = 23;
+		System.out.println(a+b1);
+	}
+}
